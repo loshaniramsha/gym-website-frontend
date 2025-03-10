ef1f55a1cd41ad3c688c26057d0f7feea1948495
loshaniramsha <loshanikottagoda@gmail.com>
2025-03-11 01:07:24 +0530

    Fixed responsive design for hero section buttons and improved layout adjustments.

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -1,8 +1,25 @@
+
 const hero=()=>{
     return(
-        <div>
+        <section className="hero">
 
-        </div>
+            <div className="content">
+                <div className="header">ELITE EDGE FITNESS</div>
+                <div className="title">
+                    <h1>LET'S</h1>
+                    <h1>GET</h1>
+                    <h1>MOVING</h1>
+                </div>
+                <div className="sub-tittle">
+                    <p>Your Journey to Fitness Start Hero</p>
+                    <p>Unleash Your Potential</p>
+                </div>
+                <div className="button">
+                    <button>Start Your Journey</button>
+                    <button>Discover Your plan</button>
+                </div>
+            </div>
+        </section>
     )
 }
 export default hero;
