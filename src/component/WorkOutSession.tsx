0212396c2d967b589eeeb07acc4861af5d4a32e7
loshaniramsha <loshanikottagoda@gmail.com>
2025-03-11 01:55:24 +0530

    Add WorkOutSession component with image and bootcamp sessions and style

--- src/component/WorkOutSession.tsx
+++ src/component/WorkOutSession.tsx
@@ -1,8 +1,37 @@
-const workOutSession=()=>{
-    return(
-        <div>
+import gym_grl from "../../public/img/gym-grl.png";
 
-        </div>
-    )
-}
-export default workOutSession
+const WorkOutSession = () => {
+    return (
+        <section className="work_session">
+            <div className="wrapper">
+                <h1>TOP WORKOUT SESSION</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores culpa dicta dolore dolorem
+                    dolores id impedit ipsa, libero magni, maxime modi nam officia placeat quidem ratione totam vero
+                    voluptate voluptates?
+                </p>
+                <img src={gym_grl} alt="Workout Girl" />
+            </div>
+            <div className="wrapper">
+                <h1>FEATURED BOOTCAMPS</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At error nam perspiciatis tempore! Aut corporis
+                    enim illo quas quo voluptatem. Earum error facilis fugit inventore nam, odio placeat soluta voluptate?
+                </p>
+                <div className="bootcamps">
+                    {[...Array(7)].map((_, index) => (
+                        <div key={index}>
+                            <h4>Exciting Bootcamp Sessions</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque eius error eum eveniet nesciunt
+                                nihil nostrum perspiciatis porro similique! Provident qui quod unde. Asperiores consequuntur, iure?
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default WorkOutSession;
