7f5223228ff486687b6a4668c1bc19ae3d9a18b6
loshaniramsha <loshanikottagoda@gmail.com>
2025-03-11 10:01:07 +0530

    implement galley and pricing section with css

--- src/component/Gallery.tsx
+++ src/component/Gallery.tsx
@@ -1,8 +1,26 @@
-const gallery =()=>{
-    return(
-        <div>
 
-        </div>
-    )
-}
-export default gallery
+const Gallery = () => {
+    const galleryImages = [
+        "public/img/img1.png",
+        "public/img/img2.png",
+        "public/img/img3.png",
+        "public/img/img4.png",
+        "public/img/img5.png",
+        "public/img/img6.png"
+    ];
+
+    return (
+        <section className="gallery">
+            <h1>BETTER BEATS BEST</h1>
+            <div className="images">
+                {galleryImages.map((image, index) => (
+                    <div key={index}>
+                        <img src={image} alt={`Gallery Image ${index + 1}`} />
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Gallery;
--- src/component/Pricing.tsx
+++ src/component/Pricing.tsx
@@ -1,8 +1,65 @@
-const pricing=()=>{
-    return(
-        <div>
+import { Check } from "lucide-react";
+import { Link } from "react-router-dom";
 
-        </div>
-    )
-}
-export default pricing
+const Pricing = () => {
+    const pricingPlans = [
+        {
+            imgUrl: "/img/pricing-img.png",
+            title: "QUARTERLY",
+            price: 10000,
+            length: 3,
+        },
+        {
+            imgUrl: "/img/pricing-img.png",
+            title: "HALF_YEARLY",
+            price: 34000,
+            length: 6,
+        },
+        {
+            imgUrl: "/img/pricing-img.png",
+            title: "YEARLY",
+            price: 670000,
+            length: 12,
+        },
+    ];
+
+    return (
+        <section className="pricing">
+            <h1>ELITE EDGE FITNESS PLANS</h1>
+            <div className="wrapper">
+                {pricingPlans.map((plan) => (
+                    <div className="card" key={plan.title}>
+                        <img src={plan.imgUrl} alt={plan.title} />
+                        <div className="title">
+                            <h1>PACKAGE</h1>
+                            <h3>Rs {plan.price}</h3>
+                            <p>For {plan.length} Months</p>
+                        </div>
+                        <div className="description">
+                            <p>
+                                <Check /> Equipment
+                            </p>
+                            <p>
+                                <Check /> All Day Training
+                            </p>
+                            <p>
+                                <Check /> Free Restore
+                            </p>
+                            <p>
+                                <Check /> 24/7 Skilled Support
+                            </p>
+                            <p>
+                                <Check /> 20 Days Freezing Option
+                            </p>
+                            <Link to={"/"} className="join-btn">
+                                Join Now
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default Pricing;
